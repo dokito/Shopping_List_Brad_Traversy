66dce7f2299211cbb5bf2b5c8997b1d1410d5b07
dokito <kr.m.kostadinov@gmail.com>
2020-05-02 21:17:46 +0300

    make it better

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: "mongodb+srv://user:user@shoppinglist-2xuzw.mongodb.net/shopping_list?retryWrites=true&w=majority"
+};
+
+// "mongodb+srv://user:user@shoppinglist-2xuzw.mongodb.net/test?retryWrites=true&w=majority"
